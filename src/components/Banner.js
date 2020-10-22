c54db3f61a76cfd6aabc5a805b0ec7680a8bf68b
Michael Knott <knottweb@gmail.com>
2020-10-22 16:11:23 +0100

    update packages

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,10 +18,9 @@ function Banner() {
     }
     fetchData();
   }, []);
-
-  function truncate(str, n) {
+  const truncate = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
+  };
 
   return (
     <header

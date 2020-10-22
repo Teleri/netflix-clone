4ba705557769fc884702405a2f3d34aacbaaf3d1
Michael Knott <knottweb@gmail.com>
2020-10-22 16:07:57 +0100

    Add video trailer support using external libraries

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import YouTube from "react-youtube";
 import axios from "../axios";
 import "../styles/Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [state, setTrailerUrl] = useState("");
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -25,6 +27,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("test"));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -32,6 +47,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
